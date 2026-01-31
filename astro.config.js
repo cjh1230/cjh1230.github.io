a4c497489836d7205219c718efbad1186a46591f
cin <2123277675@qq.com>
2026-02-01 03:05:09 +0800

    feat: test

--- astro.config.js
+++ astro.config.js
@@ -19,7 +19,7 @@ import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cjh1230.github.io',
+  site: 'https://drowsyindra.github.io',
   integrations: [tailwind(), react(), sitemap()],
   markdown: {
     syntaxHighlight: false,
